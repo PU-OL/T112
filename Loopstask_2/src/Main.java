85d0c0ca129d756a6fa86f9cde9af0e05b14bc60
Philipp Uhlendorf <pu-ol@gmx.de>
2025-12-05 09:01:34 +0100

    Add Looptasks_2 part 5 finding prime numbers

--- Loopstask_2/src/Main.java
+++ Loopstask_2/src/Main.java
@@ -6,12 +6,13 @@ public class Main {
         aufgabe2();
         aufgabe3();
         aufgabe4();
+        aufgabe5();
 
     }
 
     private static void aufgabe1(){
-        int a = 1;
-        int b = 2;
+        int a = 10;
+        int b = 20;
         System.out.println("----------");
         System.out.println("Aufgabe 1: ");
         for(int c = a; c <= b; c++){
@@ -21,8 +22,8 @@ public class Main {
     }
 
     private static void aufgabe2(){
-        int a = 1;
-        int b = 20;
+        int a = 20;
+        int b = 30;
         System.out.println("----------");
         System.out.println("Aufgabe 2: ");
         if(a != b){
@@ -40,11 +41,11 @@ public class Main {
     }
 
     private static void aufgabe3(){
-        int n = 5;
         int counter = 0;
         System.out.println("----------");
         System.out.println("Aufgabe 3: ");
         Random r = new Random();
+        int n = r.nextInt(5)+2;
         int arrayLength = r.nextInt(20) + 20;
         int array[] = new int[arrayLength];
         for (int i=0; i<arrayLength; i++){
@@ -76,6 +77,25 @@ public class Main {
                 System.out.print("*");
             }
             System.out.println();
+        }
+        System.out.println("----------");
+    }
+
+    private static void aufgabe5(){
+        int p = 42;
+        int countprim = 0;
+        System.out.println("----------");
+        System.out.println("Aufgabe 5: ");
+        for(int i = 1; i <= p; i++){
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    countprim++;
+                }
+            }
+            if(countprim == 2){
+                System.out.println(i);
+            }
+            countprim = 0;
         }
         System.out.println("----------");
     }
