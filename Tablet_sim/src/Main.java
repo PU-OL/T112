951a9ad4a3b2a9220c93d9522fd78eec3aa7ed8e
PU-OL <pu-ol@gmx.de>
2026-03-25 22:19:57 +0100

    Fix small bugs

--- Tablet_sim/src/Main.java
+++ Tablet_sim/src/Main.java
@@ -5,63 +5,85 @@ public class Main {
     public static void main(String[] args) {
         double displayDiagonale = 11;
         int akkuLadung = 20;
-        boolean isOn = false;
+        boolean isOn = true;
 
+        akkuLadung = akkuAufladen(akkuLadung, 30);
         akkuLadung = akkuAufladen(akkuLadung, 100);
-        akkuLadung = akkuAufladen(akkuLadung, 80);
+        akkuLadung = akkuAufladen(akkuLadung, 111);
+        akkuLadung = akkuAufladen(akkuLadung, 40);
 
         isOn = switchOn(isOn);
         isOn = switchOff(isOn);
     }
 
-    private static int akkuAufladen(int akku, int target){
-        if(target <= 100){
-            System.out.println("Laden bis " + target + "% gestartet.");
-            for (int i = akku+1; i <= target; i++) {
-                int filled = (i * 30)/100;
-                //String bar = "[🚂" + "🚋".repeat(filled) +  " ".repeat(10-filled) + "]";
-                String bar = "[" + "-".repeat(filled) + "🛬" + " ".repeat(30-filled) + "]";
-
-                System.out.print("\r\033[2K");
-                System.out.print("Akkustand: " + i + "%" + " ||" + bar);
-
-                akku = i;
-                if(i < 50) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {}
-                }else{
-                    try{
-                        Thread.sleep(2500);
-                    }catch (InterruptedException e){}
-                }
-            }
-            System.out.println();
-            System.out.println("Laden beendet mit: " + akku + "%");
+    private static int akkuAufladen(int akku, int target) {
+        if (akku >= target) {
+            System.out.println("\033[31m Der Akkustand (" + akku + "%) ist größer als die Ziel Ladung mit " + target + "%");
+            brL(25);
             return akku;
         }else{
-            System.err.println("Der Aktuelle Zielwert " + target + "% überschreitet das Maximum von 100%");
-            return akku;
+            if (target <= 100) {
+                System.out.println("\033[32m Laden bis " + target + "% gestartet.");
+                for (int i = akku + 1; i <= target; i++) {
+                    int filled = (i * 30) / 100;
+                    //String bar = "[🚂" + "🚋".repeat(filled) +  " ".repeat(10-filled) + "]";
+                    String bar = "[" + "=".repeat(filled) + "🛬" + " ".repeat(30 - filled) + "]";
+
+                    System.out.print("\r\033[2K");
+                    System.out.print(" \033[5;36m Akkustand: " + i + "%" + " ||" + bar);
+
+                    akku = i;
+                    if (i < 75) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                        }
+                    } else {
+                        try {
+                            Thread.sleep(2500);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+                System.out.println();
+                System.out.println("\033[32m Laden beendet mit: " + akku + "%");
+                brL(25);
+                return akku;
+            } else {
+                System.out.println("\033[31m Der Aktuelle Zielwert " + target + "% überschreitet das Maximum von 100%");
+                brL(25);
+                return akku;
+            }
         }
     }
 
     private static boolean switchOn(boolean status){
         if(status){
-            System.err.println("Gerät ist bereits angeschaltet");
-            return status;
+            System.out.println("\033[31m Das Gerät ist bereits angeschaltet");
         }else {
-            System.out.println("Gerät angeschaltet worden");
-            return status;
+            System.out.println("\033[32m Das Gerät ist angeschaltet worden");
         }
+        brL(25);
+        return status;
     }
 
     private static boolean switchOff(boolean status){
-        if(status){
-            System.err.println("Gerät ist bereits abgeschaltet");
-            return status;
+        if(!status){
+            System.out.println("\033[31m Das Gerät ist bereits abgeschaltet");
+            brL(25);
         }else {
-            System.out.println("Gerät abgeschaltet worden");
-            return status;
+            System.out.println("\033[32m Das Gerät ist abgeschaltet worden");
+        }
+        return status;
+    }
+
+    private static void brL(int lenght){
+        System.out.print("\033[0m");
+        if(lenght > 0) {
+            for (int i = 0; i < lenght; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
         }
     }
 }
